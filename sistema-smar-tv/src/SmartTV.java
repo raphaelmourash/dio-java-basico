20706fb401b9156215a45264c2fa1747ef401ea6
Raphael Moura <raphael.moura.sh@gmail.com>
2024-05-31 20:37:10 -0300

    atualização  aulas

---
+++ sistema-smar-tv/src/SmartTV.java
@@ -0,0 +1,43 @@
+public class SmartTV {
+    
+boolean ligada=false;
+int canal = 1;
+int volume = 25;
+public void mudarCanal(int novoCanal){
+    canal = novoCanal;
+}
+
+public void aumentarCanal(){
+    canal++;
+    
+    System.out.println("Aumentando Canal para: "+canal);
+    }
+    
+    public void diminuirCanal(){
+        canal--;
+    
+        System.out.println("Diminuindo  o Canal para: "+canal);
+        }
+
+        
+public void aumentarVolume(){
+volume++;
+
+System.out.println("Aumentando o volume para: "+volume);
+}
+
+public void diminuirVolume(){
+    volume--;
+
+    System.out.println("Diminuindo  o volume para: "+volume);
+    }
+
+public void ligar(){
+    ligada = true;
+}
+
+public void desligar(){
+    ligada = false;
+}
+
+}
---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,17 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+        
+        int a, b;
+                a = 5;
+                b = 6;
+
+String resultado = (a==b) ? "verdadeiro" : "false";
+
+System.out.println(resultado);
+    
+
+
+    }
+    
+}

7e244d75cb16f1a15808aa1d9e718396929bdcf3
Raphael Moura <raphael.moura.sh@gmail.com>
2024-06-10 12:56:53 -0300

    conta banco

--- CONTA-BANCO/src/ContaTerminal.java
+++ CONTA-BANCO/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitando os dados do usuário
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta:");
+        String numeroConta = scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do Cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo:");
+        double saldo = scanner.nextDouble();
+
+        // Exibindo a mensagem com os dados coletados
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo %.2f já está disponível para saque.%n", 
+                          nomeCliente, agencia, numeroConta, saldo);
+
+        // Fechando o scanner
+        scanner.close();
     }
 }
+    
+
